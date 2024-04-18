351622c2399d35e5c2c3c97b64d828a8ab9454a6
laura-D21126682 <D21126682@mytudublin.ie>
2024-04-18 01:30:57 +0000

    Relative path attempt 12

--- unit_tests/first-attempt-unit-test.py
+++ unit_tests/first-attempt-unit-test.py
@@ -1,6 +1,9 @@
 from ctypes import *
+import os
 
-so_file = "./unit_tests/multiplyByThree.so"
+base_path = os.path.dirname(os.path.abspath(__file__))  # Gets the directory of the script
+so_file = os.path.join(base_path, 'multiplyByThree.so')
+
 
 function_test = CDLL(so_file)
 
