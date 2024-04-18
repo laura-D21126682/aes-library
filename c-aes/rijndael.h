c26c2786f4b2596cebfa569e17697d84824e2244
laura-D21126682 <D21126682@mytudublin.ie>
2024-04-18 01:17:43 +0000

    Eighth Attempt

--- c-aes/rijndael.h
+++ c-aes/rijndael.h
@@ -14,6 +14,7 @@
  * entry point to the library for programmes hoping to use it to
  * encrypt or decrypt data)
  */
+
 unsigned char *aes_encrypt_block(unsigned char *plaintext, unsigned char *key);
 unsigned char *aes_decrypt_block(unsigned char *ciphertext, unsigned char *key);
 
