75027edc7dbd09b3e3f6d95cbcbf5a355e1f4676
laura-D21126682 <D21126682@mytudublin.ie>
2024-05-11 01:03:40 +0000

    Logging path

--- unit-tests/aes-tests.py
+++ unit-tests/aes-tests.py
@@ -8,9 +8,12 @@ import secrets
 # Path for AES python submodule  
 python_aes_path = Path(__file__).resolve().parent.parent / 'python-aes'
 sys.path.append(str(python_aes_path))
+
+print("abs path here: +++++++++++++++++++++++++++++++++++", os.path.abspath(__file__))
+
 import aes as aes_python
 
-# print("abs path here: +++++++++++++++++++++++++++++++++++", os.path.abspath(__file__))
+
 
 # Path for .so file (shared object)
 base_path = Path(__file__).resolve().parent
