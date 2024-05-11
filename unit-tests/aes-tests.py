3a5b5cc49cc324e16218f738a2f52d76ee9151e5
laura-D21126682 <D21126682@mytudublin.ie>
2024-05-11 17:12:28 +0000

    Inverse Shift-Rows Unit Test

--- unit-tests/aes-tests.py
+++ unit-tests/aes-tests.py
@@ -103,30 +103,29 @@ class TestAESMethods(unittest.TestCase):
       print("Test Restults: ", test_results)
 
 
-  # '''
-  # Invert Shift-Row Unit Tests
-  # '''
-  # def test_invert_shift_rows(self):
-  #   buffers = random_buffer_generator()
-  #   test_results = []
-  #   for buffer in buffers:
-  #     # Convert buffer to C and Python expected formats
-  #     c_buffer = create_string_buffer(buffer, 16).raw; # C expects 16 byte flat array
-  #     py_buffer = [list(buffer[i * 4:(i + 1) * 4]) for i in range(4)] # Python expects 4*4 matrix
+  '''
+  Invert Shift-Row Unit Tests
+  '''
+  def test_invert_shift_rows(self):
+    buffers = random_buffer_generator()
+    test_results = []
+    for buffer in buffers:
+      # transform buffers
+      c_buffer = create_string_buffer(buffer, 16).raw;
+      py_buffer = [list(buffer[i * 4:(i + 1) * 4]) for i in range(4)] 
       
-  #     # Call invert shift_row functions
-  #     aes_c.invert_shift_rows(c_buffer)
-  #     aes_python.inv_shift_rows(py_buffer)
+      # Call invert shift_rows
+      aes_c.invert_shift_rows(c_buffer)
+      aes_python.inv_shift_rows(py_buffer)
 
-  #     # Convert results data types for unit test comparisonn
-  #     c_result = list(c_buffer) # converts to python list
-  #     py_result = sum(py_buffer, []) # flattens 4*4 matrix to python list
+      # transform results for unit test
+      c_result = list(c_buffer) 
+      py_result = sum(py_buffer, []) 
 
-  #     test_results.append((c_result == py_result))
-
-  #     self.assertEqual(c_result, py_result, "C and Python results should be the same")
+      test_results.append((c_result == py_result))
+      self.assertEqual(c_result, py_result, "C and Python results should be the same")
     
-  #   print("Test Restults: ", test_results)
+    print("Test Restults: ", test_results)
 
   
   # '''
